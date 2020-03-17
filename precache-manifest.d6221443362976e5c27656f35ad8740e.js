b72b154ef5fdc284f8b705917287cb82a05111d7
niiccolas <40167702+niiccolas@users.noreply.github.com>
2020-03-17 19:47:36 +0100

    Updates

---
+++ precache-manifest.d6221443362976e5c27656f35ad8740e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dec47cabc998b9035080992423b652da",
+    "url": "/zolonda/index.html"
+  },
+  {
+    "revision": "a3746fe4c7ba9cdecad4",
+    "url": "/zolonda/static/css/main.ad7c1737.chunk.css"
+  },
+  {
+    "revision": "960877756950eb170ff4",
+    "url": "/zolonda/static/js/2.c4e63627.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/zolonda/static/js/2.c4e63627.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a3746fe4c7ba9cdecad4",
+    "url": "/zolonda/static/js/main.32261d85.chunk.js"
+  },
+  {
+    "revision": "36987e3ed33aca9407a4",
+    "url": "/zolonda/static/js/runtime-main.2f9a4dfa.js"
+  }
+]);
